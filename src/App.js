5b44c3dd82ab0bc874acc1c28c88fbe60da71f8a
himel8 <himelkhan290@gmail.com>
2023-11-21 02:56:41 +0600

    added new

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Feed from "./components/Feed";
 import Header from "./components/Header";
 import SearchResult from "./components/SearchResult";
-import Test from "./components/Test";
 import VideoDetails from "./components/VideoDetails";
 import { AppContext } from "./context/contextApi";
 
@@ -13,8 +13,8 @@ function App() {
         <div className="flex flex-col h-full">
           <Header />
           <Routes>
-            {/* <Route path="/" element={<Feed />} /> */}
-            <Route path="/" element={<Test />} />
+            <Route path="/" element={<Feed />} />
+            {/* <Route path="/" element={<Test />} /> */}
             <Route
               path="/searchResult/:searchQuery"
               element={<SearchResult />}
